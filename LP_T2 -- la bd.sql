53037e07061121dd78e6a9d5e1a7f58d114d00f9
AdrianMorante <146242310+AdrianMorante@users.noreply.github.com>
2024-06-24 16:17:26 -0500

    Add files via upload

---
+++ LP_T2 -- la bd.sql
@@ -0,0 +1,12 @@
+create database BDT2_AdrianMorante;
+use BDT2_AdrianMorante;
+select * from producto;
+
+/*la tabla se crea dentro del spring tools 4 acuerdate papu*/
+
+INSERT INTO producto( name, Stock, marca) VALUES
+( 'Arroz', '100', 'Costeño'),
+( 'Azucar', '200', 'Azucar Rubia'),
+('Inka kola', '150', 'Inka kola');
+
+/*Adrian Morante Quispe*/
